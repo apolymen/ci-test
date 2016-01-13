391c3ce39f90101254af5352c48de7eb679c67b0
apolymen <apolymen@gmail.com>
2016-01-13 15:03:17 +0200

    Validation rules and datepicker

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,38 +4,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('retrieve_model');
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('retrieve_model');
+	}
 
-    public function index() {
-        //$this->one();
-        $this->two();
-    }
+	public function index() {
+		//$this->one();
+		$this->two();
+	}
 
-    public function one() {
-        $query = $this->retrieve_model->get_fields();
-        $data['people'] = $query->result();
-        $this->load->view('home_view', $data);
-    }
+	public function one() {
+		$query = $this->retrieve_model->get_fields();
+		$data['people'] = $query->result();
+		$this->load->view('home_view', $data);
+	}
 
-    public function two() {
-        $this->table->set_heading('FirstName', 'LastName', 'Country');
-        $query = $this->retrieve_model->get_fields();
-        $data['people'] = $this->table->generate($query);
-        $this->load->view('alt_home_view', $data);
-    }
+	public function two() {
+		$this->table->set_heading('FirstName', 'LastName', 'Country');
+		$query = $this->retrieve_model->get_fields();
+		$data['people'] = $this->table->generate($query);
+		$this->load->view('alt_home_view', $data);
+	}
 
-    public function input() {
-        $this->form_validation->set_rules('amount', 'Amount', 'trim|required|regex_match[/^[0-9]+,[0-9]+$/]');
+	public function input() {
+		$this->form_validation->set_rules('xDate', 'Date', 'required');
+		$this->form_validation->set_rules('amount', 'Amount', 'trim|regex_match[/^\d+(,\d{1,3})?$/]');
+		$this->form_validation->set_rules('description', 'Description', 'trim|max_length[255]');
 
-        if ($this->form_validation->run() == FALSE) {
-            //fail validation
-            $this->load->view('input_view');
-        } else {
-            echo "Success";
-        }
-    }
+		if ($this->form_validation->run() == FALSE) {
+			//fail validation
+			$this->load->view('input_view');
+		} else {
+			$amount = $this->input->post('amount');
+			$val = floatval(str_replace(',', '.', $amount));
+			echo "Success<br>" . $amount . " --> " . $val;
+			echo "<br>" . $this->input->post('xDate');
+		}
+	}
 
 }
--- application/views/input_view.php
+++ application/views/input_view.php
@@ -1,21 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Input</title>
+	<head>
+		<meta charset="UTF-8">
+		<title>Input</title>
+
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.css" rel="stylesheet">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
     </head>
-    <body>
-        <?php echo form_open("input"); ?>
-        <div>
-            <label for="amount">Amount</label>
-        </div>
-        <div>
-            <input type="text" name="amount" value="<?php echo set_value('amount'); ?>" />
-            <span><?php echo form_error('amount'); ?></span>
-        </div>
-        <div>
-            <input type="submit" name="Submit" />
-        </div>
-        <?php echo form_close(); ?>
-    </body>
+	<body>
+		<?php echo form_open("input"); ?>
+
+		<div>
+			<label for="xDate">Date</label>
+		</div>
+		<div>
+			<input id="xDate" type="text" name="xDate" placeholder="Date" value="<?php echo set_value('xDate'); ?>" />
+			<span><?php echo form_error('xDate'); ?></span>
+		</div>
+
+		<div>
+			<label for="amount">Amount</label>
+		</div>
+		<div>
+			<input type="text" name="amount" placeholder="Amount (max 3 decimals)" value="<?php echo set_value('amount'); ?>" />
+			<span><?php echo form_error('amount'); ?></span>
+		</div>
+
+		<div>
+			<label for="description">Description</label>
+		</div>
+		<div>
+			<input type="text" name="description" placeholder="Short description" value="<?php echo set_value('description'); ?>" />
+			<span><?php echo form_error('description'); ?></span>
+		</div>
+
+		<div>
+			<input type="submit" name="Submit" />
+		</div>
+
+		<?php echo form_close(); ?>
+
+
+      <script type="text/javascript">
+			//load datepicker control onfocus
+			$(function () {
+				$("#xDate").datepicker(
+					{ dateFormat: "yy-mm-dd" }
+				);
+			});
+		</script>
+
+	</body>
 </html>
